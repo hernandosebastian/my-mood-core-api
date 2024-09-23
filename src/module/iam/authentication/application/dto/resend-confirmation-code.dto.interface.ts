790dde827f18e78489a37b8a9759715cf9fb436a
Mipmipp <sebastianhernandoh@gmail.com>
2024-09-23 17:47:13 -0300

    Fix: An interface declaring no members is equivalent to its supertype.

--- src/module/iam/authentication/application/dto/resend-confirmation-code.dto.interface.ts
+++ src/module/iam/authentication/application/dto/resend-confirmation-code.dto.interface.ts
@@ -1,3 +1,3 @@
 import { IForgotPasswordDto } from '@iam/authentication/application/dto/forgot-password.dto.interface';
 
-export interface IResendConfirmationCodeDto extends IForgotPasswordDto {}
+export type IResendConfirmationCodeDto = IForgotPasswordDto;
