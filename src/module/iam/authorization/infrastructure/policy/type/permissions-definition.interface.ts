7b2e81cc1d0e76943382816d17d597100361bf11
Mipmipp <sebastianhernandoh@gmail.com>
2024-09-23 17:52:38 -0300

    Fix: An interface declaring no members is equivalent to its supertype.

--- src/module/iam/authorization/infrastructure/policy/type/permissions-definition.interface.ts
+++ src/module/iam/authorization/infrastructure/policy/type/permissions-definition.interface.ts
@@ -1,5 +1,6 @@
 import { AppRole } from '@iam/authorization/domain/app-role.enum';
 import { DefinePermissions } from '@iam/authorization/infrastructure/policy/type/define-permissions.type';
 
-export interface IPermissionsDefinition
-  extends Partial<Record<AppRole, DefinePermissions>> {}
+export type IPermissionsDefinition = Partial<
+  Record<AppRole, DefinePermissions>
+>;
