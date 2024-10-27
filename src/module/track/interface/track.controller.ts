4c5bca70056de47330a3391eccea351b4279c60d
Mipmipp <sebastianhernandoh@gmail.com>
2024-10-27 00:49:38 -0300

    Update controller

--- src/module/track/interface/track.controller.ts
+++ src/module/track/interface/track.controller.ts
@@ -6,12 +6,16 @@ import {
   Param,
   Patch,
   Post,
+  Request,
   UseGuards,
 } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { Request as ExpressRequest } from 'express';
 
 import { Policies } from '@iam/authorization/infrastructure/policy/decorator/policy.decorator';
 import { PoliciesGuard } from '@iam/authorization/infrastructure/policy/guard/policy.guard';
+import { User } from '@iam/user/domain/user.entity';
+import { getCurrentUserFromRequest } from '@iam/user/domain/util/getCurrentUserFromRequest.util';
 
 import { CreateTrackDto } from '@/module/track/application/dto/create-track.dto';
 import { GetTracksByDateRangeDto } from '@/module/track/application/dto/get-tracks-by-date-range.dto';
@@ -29,15 +33,17 @@ import { TrackService } from '@/module/track/application/service/track.service';
 export class TrackController {
   constructor(private readonly trackService: TrackService) {}
 
-  @Get('/by-date-range/:userId')
+  @Get('/by-date-range')
   @Policies(ReadTrackPolicyHandler)
   async getTracksByDateRange(
     @Body() getTracksByDateRangeDto: GetTracksByDateRangeDto,
-    @Param('userId') userId: number,
+    @Request() req: ExpressRequest,
   ): Promise<TrackResponseDto[]> {
+    const currentUser = this.getCurrentUser(req);
+
     return this.trackService.getTracksByDateRange(
       getTracksByDateRangeDto,
-      userId,
+      currentUser.id,
     );
   }
 
@@ -45,8 +51,10 @@ export class TrackController {
   @Policies(CreateTrackPolicyHandler)
   async saveOne(
     @Body() createTrackDto: CreateTrackDto,
+    @Request() req: ExpressRequest,
   ): Promise<TrackResponseDto> {
-    return this.trackService.saveOne(createTrackDto);
+    const currentUser = this.getCurrentUser(req);
+    return this.trackService.saveOne(createTrackDto, currentUser.id);
   }
 
   @Patch(':id')
@@ -54,13 +62,24 @@ export class TrackController {
   async updateOneOrFail(
     @Param('id') id: number,
     @Body() updateTrackDto: UpdateTrackDto,
+    @Request() req: ExpressRequest,
   ): Promise<TrackResponseDto> {
-    return this.trackService.updateOneOrFail(id, updateTrackDto);
+    const currentUser = this.getCurrentUser(req);
+
+    return this.trackService.updateOneOrFail(
+      id,
+      updateTrackDto,
+      currentUser.id,
+    );
   }
 
   @Policies(DeleteTrackPolicyHandler)
   @Delete(':id')
   async deleteOneOrFail(@Param('id') id: number): Promise<void> {
     return this.trackService.deleteOneOrFail(id);
+  }
+
+  private getCurrentUser(request: ExpressRequest): User {
+    return getCurrentUserFromRequest(request);
   }
 }
