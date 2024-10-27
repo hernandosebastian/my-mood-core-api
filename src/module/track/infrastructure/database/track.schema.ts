4aa86f323ce57095be01ebe6d75e561d7d29afc7
Mipmipp <sebastianhernandoh@gmail.com>
2024-10-26 22:33:01 -0300

    Add track schema and entity

---
+++ src/module/track/infrastructure/database/track.schema.ts
@@ -0,0 +1,37 @@
+import { EntitySchema } from 'typeorm';
+
+import { withBaseSchemaColumns } from '@common/base/infrastructure/database/base.schema';
+
+import { Track } from '@/module/track/domain/track.entity';
+
+export const TrackSchema = new EntitySchema<Track>({
+  name: 'Track',
+  target: Track,
+  tableName: 'track',
+  columns: withBaseSchemaColumns({
+    title: {
+      type: String,
+    },
+    description: {
+      type: String,
+      nullable: true,
+    },
+    date: {
+      type: Date,
+    },
+    ownerId: {
+      type: Number,
+      name: 'fk_owner_id',
+    },
+  }),
+  relations: {
+    owner: {
+      type: 'many-to-one',
+      target: 'User',
+      inverseSide: 'tracks',
+      joinColumn: {
+        name: 'fk_owner_id',
+      },
+    },
+  },
+});
