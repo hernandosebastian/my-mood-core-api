53dcd8eb47f9890182f71f5dd115d42556150551
Mipmipp <sebastianhernandoh@gmail.com>
2024-10-26 22:29:21 -0300

    Delete user dtos unused

--- src/module/iam/user/application/dto/user-filter-query-params.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiPropertyOptional } from '@nestjs/swagger';
-import { IsOptional, IsString } from 'class-validator';
-
-export class UserFilterQueryParamsDto {
-  @ApiPropertyOptional()
-  @IsString()
-  @IsOptional()
-  username?: string;
-}
