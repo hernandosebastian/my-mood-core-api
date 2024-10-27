53567f6918b372839b3872a3347e52f607aa9106
Mipmipp <sebastianhernandoh@gmail.com>
2024-10-26 22:34:23 -0300

    Add track policies

---
+++ src/module/track/application/policy/create-track-policy.handler.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@nestjs/common';
+import { Request } from 'express';
+
+import { REQUEST_USER_KEY } from '@iam/authentication/authentication.constants';
+import { AuthorizationService } from '@iam/authorization/application/service/authorization.service';
+import { AppAction } from '@iam/authorization/domain/app-action.enum';
+import { IPolicyHandler } from '@iam/authorization/infrastructure/policy/handler/policy-handler.interface';
+import { PolicyHandlerStorage } from '@iam/authorization/infrastructure/policy/storage/policies-handler.storage';
+import { User } from '@iam/user/domain/user.entity';
+
+import { CreateTrackDto } from '@/module/track/application/dto/create-track.dto';
+import { Track } from '@/module/track/domain/track.entity';
+
+@Injectable()
+export class CreateTrackPolicyHandler implements IPolicyHandler {
+  private readonly action = AppAction.Create;
+
+  constructor(
+    private readonly policyHandlerStorage: PolicyHandlerStorage,
+    private readonly authorizationService: AuthorizationService,
+  ) {
+    this.policyHandlerStorage.add(CreateTrackPolicyHandler, this);
+  }
+
+  async handle(request: Request): Promise<void> {
+    if (!this.hasPermission(request)) {
+      throw new Error(
+        `You are not allowed to ${this.action.toUpperCase()} this resource`,
+      );
+    }
+  }
+
+  private hasPermission(request: Request): boolean {
+    const currentUser = this.getCurrentUser(request);
+    const requestBody: CreateTrackDto = request.body;
+
+    return (
+      this.authorizationService.isAllowed(currentUser, this.action, Track) &&
+      currentUser.id === requestBody.ownerId
+    );
+  }
+
+  private getCurrentUser(request: Request): User {
+    return request[REQUEST_USER_KEY];
+  }
+}
---
+++ src/module/track/application/policy/delete-track-policy.handler.ts
@@ -0,0 +1,57 @@
+import { Inject, Injectable, Type } from '@nestjs/common';
+import { Request } from 'express';
+
+import { REQUEST_USER_KEY } from '@iam/authentication/authentication.constants';
+import { AuthorizationService } from '@iam/authorization/application/service/authorization.service';
+import { AppAction } from '@iam/authorization/domain/app-action.enum';
+import { IPolicyHandler } from '@iam/authorization/infrastructure/policy/handler/policy-handler.interface';
+import { PolicyHandlerStorage } from '@iam/authorization/infrastructure/policy/storage/policies-handler.storage';
+import { User } from '@iam/user/domain/user.entity';
+
+import {
+  ITrackRepository,
+  TRACK_REPOSITORY_KEY,
+} from '@/module/track/application/repository/track.repository.interface';
+import { Track } from '@/module/track/domain/track.entity';
+
+@Injectable()
+export class DeleteTrackPolicyHandler implements IPolicyHandler {
+  private readonly action = AppAction.Delete;
+
+  constructor(
+    private readonly policyHandlerStorage: PolicyHandlerStorage,
+    private readonly authorizationService: AuthorizationService,
+    @Inject(TRACK_REPOSITORY_KEY)
+    private readonly trackRepository: ITrackRepository,
+  ) {
+    this.policyHandlerStorage.add(DeleteTrackPolicyHandler, this);
+  }
+
+  async handle(request: Request): Promise<void> {
+    const currentUser = this.getCurrentUser(request);
+    const subjectOrSubjectCls = await this.getSubject(request);
+
+    const isAllowed = this.authorizationService.isAllowed(
+      currentUser,
+      this.action,
+      subjectOrSubjectCls,
+    );
+
+    if (!isAllowed) {
+      throw new Error(
+        `You are not allowed to ${this.action.toUpperCase()} this resource`,
+      );
+    }
+  }
+
+  private getCurrentUser(request: Request): User {
+    return request[REQUEST_USER_KEY];
+  }
+
+  private async getSubject(request: Request): Promise<Track | Type<Track>> {
+    const searchParam = request.params['id'];
+    const subjectId = searchParam ? parseInt(searchParam) : undefined;
+    const subject = await this.trackRepository.getOneById(subjectId);
+    return subject ?? Track;
+  }
+}
