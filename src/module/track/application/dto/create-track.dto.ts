0f9c6454824471ee73ff4c231cb7690ea9ddf9cb
Mipmipp <sebastianhernandoh@gmail.com>
2024-10-31 23:43:12 -0300

    Improve updateTrack and createTrack with maxLength

--- src/module/track/application/dto/create-track.dto.ts
+++ src/module/track/application/dto/create-track.dto.ts
@@ -1,5 +1,11 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import { IsDate, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import {
+  IsDate,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+} from 'class-validator';
 
 import { ICreateTrackDto } from '@/module/track/application/dto/create-track.dto.interface';
 
@@ -7,11 +13,13 @@ export class CreateTrackDto implements Partial<ICreateTrackDto> {
   @ApiProperty()
   @IsString()
   @IsNotEmpty()
+  @MaxLength(100)
   title: string;
 
   @ApiPropertyOptional()
   @IsString()
   @IsOptional()
+  @MaxLength(200)
   description?: string;
 
   @ApiProperty()
--- src/module/track/application/dto/update-track.dto.ts
+++ src/module/track/application/dto/update-track.dto.ts
@@ -1,14 +1,16 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
-import { IsOptional, IsString } from 'class-validator';
+import { IsOptional, IsString, MaxLength } from 'class-validator';
 
 export class UpdateTrackDto {
   @ApiPropertyOptional()
   @IsString()
   @IsOptional()
+  @MaxLength(100)
   title?: string;
 
   @ApiPropertyOptional()
   @IsString()
   @IsOptional()
+  @MaxLength(200)
   description?: string;
 }
