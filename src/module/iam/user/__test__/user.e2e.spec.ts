c019bf12b4244847449ec6dc4ff63b1592207cc2
Mipmipp <sebastianhernandoh@gmail.com>
2024-11-14 20:31:22 -0300

    Add tests

--- src/module/iam/user/__test__/user.e2e.spec.ts
+++ src/module/iam/user/__test__/user.e2e.spec.ts
@@ -6,6 +6,8 @@ import { loadFixtures } from '@data/util/fixture-loader';
 import { setupApp } from '@config/app.config';
 import { datasourceOptions } from '@config/orm.config';
 
+import { UpdateUserDto } from '@iam/user/application/dto/update-user.dto';
+
 import { testModuleBootstrapper } from '@test/test.module.bootstrapper';
 import { createAccessToken } from '@test/test.util';
 
@@ -50,6 +52,66 @@ describe('User Module', () => {
       await request(app.getHttpServer())
         .get('/api/v1/user/me')
         .expect(HttpStatus.UNAUTHORIZED);
+    });
+  });
+
+  describe('PATCH - /user/me', () => {
+    it('should update a user with valid data', async () => {
+      const updateUserDto: UpdateUserDto = {
+        nickname: 'updated-nickname',
+        avatarSrc: 'updated-avatar-src',
+      };
+
+      await request(app.getHttpServer())
+        .patch('/api/v1/user/me')
+        .auth(adminToken, { type: 'bearer' })
+        .send(updateUserDto)
+        .expect(HttpStatus.OK)
+        .then(({ body }) => {
+          expect(body).toEqual({
+            id: expect.any(Number),
+            username: expect.any(String),
+            nickname: 'updated-nickname',
+            avatarSrc: 'updated-avatar-src',
+            externalId: expect.any(String),
+            roles: expect.arrayContaining([expect.any(String)]),
+            createdAt: expect.any(String),
+            updatedAt: expect.any(String),
+            deletedAt: null,
+          });
+        });
+    });
+
+    it('should return a validation error for invalid nickname', async () => {
+      const updateUserDto: UpdateUserDto = {
+        nickname: 'a'.repeat(50),
+      };
+
+      await request(app.getHttpServer())
+        .patch('/api/v1/user/me')
+        .auth(adminToken, { type: 'bearer' })
+        .send(updateUserDto)
+        .expect(HttpStatus.BAD_REQUEST)
+        .then(({ body }) => {
+          expect(body.message).toEqual([
+            'nickname must be shorter than or equal to 35 characters',
+          ]);
+        });
+    });
+
+    it('should return a validation error if nickname is already taken', async () => {
+      const updateUserDto: UpdateUserDto = {
+        nickname: 'regular',
+      };
+
+      await request(app.getHttpServer())
+        .patch('/api/v1/user/me')
+        .auth(adminToken, { type: 'bearer' })
+        .send(updateUserDto)
+        .expect(HttpStatus.CONFLICT)
+        .then(({ body }) => {
+          expect(body.message).toEqual('regular nickname is already taken');
+        });
     });
   });
 });
