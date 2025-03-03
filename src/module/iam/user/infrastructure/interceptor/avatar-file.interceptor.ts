639325b971602bb45d8999333cccf1960dd7b11b
hernandosebastian <sebastianhernando.dev@gmail.com>
2025-03-03 15:59:35 -0300

    feature: add a interceptor for avatar files

---
+++ src/module/iam/user/infrastructure/interceptor/avatar-file.interceptor.ts
@@ -0,0 +1,62 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+  Type,
+  UnsupportedMediaTypeException,
+  mixin,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { MulterOptions } from '@nestjs/platform-express/multer/interfaces/multer-options.interface';
+import { Observable } from 'rxjs';
+
+const AVATAR_FIELD_NAME = 'avatar';
+
+export function AvatarFileInterceptor(): Type<NestInterceptor> {
+  @Injectable()
+  class MixinInterceptor implements NestInterceptor {
+    private readonly ONE_MB = 1024 * 1024;
+    private readonly MAX_FILE_SIZE = this.ONE_MB;
+    private readonly ALLOWED_MIME_TYPES = [
+      'image/png',
+      'image/jpg',
+      'image/jpeg',
+    ];
+    private readonly MIME_TYPE_ERROR_MESSAGE =
+      'File type not allowed. Accepted formats: PNG, JPG, JPEG';
+
+    async intercept(
+      context: ExecutionContext,
+      next: CallHandler,
+    ): Promise<Observable<any>> {
+      const multerOptions: MulterOptions = {
+        fileFilter: (req, file, callback) => {
+          if (!this.ALLOWED_MIME_TYPES.includes(file.mimetype)) {
+            callback(
+              new UnsupportedMediaTypeException(this.MIME_TYPE_ERROR_MESSAGE),
+              false,
+            );
+          }
+
+          callback(null, true);
+        },
+        limits: {
+          fileSize: this.MAX_FILE_SIZE,
+        },
+      };
+
+      try {
+        const fileInterceptor = new (FileInterceptor(
+          AVATAR_FIELD_NAME,
+          multerOptions,
+        ))();
+        return fileInterceptor.intercept(context, next);
+      } catch (error) {
+        throw error;
+      }
+    }
+  }
+
+  return mixin(MixinInterceptor);
+}
