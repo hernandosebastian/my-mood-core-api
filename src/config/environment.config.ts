7efc9475bb90df49d6fab2cfc12eca9cf9671f0b
hernandosebastian <sebastianhernando.dev@gmail.com>
2025-03-03 16:29:17 -0300

    refactor: update variables

--- src/config/environment.config.ts
+++ src/config/environment.config.ts
@@ -8,6 +8,15 @@ export const environmentConfig = () => ({
     issuer: process.env.COGNITO_ISSUER,
     endpoint: process.env.COGNITO_ENDPOINT,
   },
+  aws: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+  s3: {
+    bucketName: process.env.S3_BUCKET_NAME,
+    region: process.env.S3_REGION,
+    localEndpoint: process.env.S3_LOCAL_ENDPOINT,
+  },
   frontend: {
     url: process.env.FRONTEND_URL,
   },
