bf1e330fe765b781b3eda140efb90db593ee8cf9
hernandosebastian <sebastianhernando.dev@gmail.com>
2025-04-05 22:17:04 -0300

    feature: add rate limit to every controller

--- src/module/health/interface/health.controller.ts
+++ src/module/health/interface/health.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { Throttle } from '@nestjs/throttler';
 
 import { AuthType } from '@iam/authentication/domain/auth-type.enum';
 import { Auth } from '@iam/authentication/infrastructure/decorator/auth.decorator';
@@ -6,6 +7,7 @@ import { Auth } from '@iam/authentication/infrastructure/decorator/auth.decorato
 @Controller('health')
 export class HealthController {
   @Get()
+  @Throttle({ default: { limit: 2, ttl: 60_000 } })
   @Auth(AuthType.None)
   async healthCheck(): Promise<{ status: string; uptime: number }> {
     return { status: 'ok', uptime: process.uptime() };
